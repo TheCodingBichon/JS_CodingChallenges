910af3758af7446d2c6b8c3f03064d1d732233b1
TheCodingBichon <93611775+TheCodingBichon@users.noreply.github.com>
2023-02-04 19:04:02 +0100

    TS setup

---
+++ Fundamentals_1/dist/Script.js
@@ -0,0 +1,8 @@
+// Coding challenge #1
+const heightMark = 1.69;
+const massMark = 78;
+const heightJohn = 1.95;
+const massJohn = 92;
+const BMIMark = massMark / Math.pow(heightMark, 2);
+const BMIJohn = massJohn / Math.pow(heightJohn, 2);
+console.log(BMIMark, BMIJohn);
---
+++ Fundamentals_1/src/Script.ts
@@ -0,0 +1,11 @@
+// Coding challenge #1
+
+const heightMark: number = 1.69;
+const massMark: number = 78;
+
+const heightJohn: number = 1.95;
+const massJohn: number = 92;
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightJohn ** 2;
+
+console.log(BMIMark, BMIJohn);
