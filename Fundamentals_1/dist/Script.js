e37d778d1145037dfafa0372e5196a6d242e27f8
TheCodingBichon <93611775+TheCodingBichon@users.noreply.github.com>
2023-02-04 19:30:02 +0100

    Coding challenge #2 - completed

--- Fundamentals_1/dist/Script.js
+++ Fundamentals_1/dist/Script.js
@@ -6,3 +6,20 @@ const massJohn = 92;
 const BMIMark = massMark / Math.pow(heightMark, 2);
 const BMIJohn = massJohn / Math.pow(heightJohn, 2);
 console.log(BMIMark, BMIJohn);
+const markHigherBMI = BMIMark > BMIJohn ? "Mark has higher BMI" : "John has higher BMI";
+console.log(markHigherBMI);
+// Coding challenge #2
+//ver 1
+let decision;
+if (BMIMark > BMIJohn) {
+    decision = `Mark has higher BMI ${BMIMark}`;
+}
+else {
+    decision = `John has higher BMI ${BMIJohn}`;
+}
+console.log(decision);
+//ver 2
+const markHigherBMI3 = BMIMark > BMIJohn
+    ? `Mark has higher BMI ${BMIMark}`
+    : `John has higher BMI ${BMIJohn}`;
+console.log(markHigherBMI3);
--- Fundamentals_1/src/Script.ts
+++ Fundamentals_1/src/Script.ts
@@ -5,7 +5,34 @@ const massMark: number = 78;
 
 const heightJohn: number = 1.95;
 const massJohn: number = 92;
+
 const BMIMark = massMark / heightMark ** 2;
 const BMIJohn = massJohn / heightJohn ** 2;
 
 console.log(BMIMark, BMIJohn);
+
+const markHigherBMI: string =
+  BMIMark > BMIJohn ? "Mark has higher BMI" : "John has higher BMI";
+
+console.log(markHigherBMI);
+
+// Coding challenge #2
+
+//ver 1
+let decision: string;
+
+if (BMIMark > BMIJohn) {
+  decision = `Mark has higher BMI ${BMIMark}`;
+} else {
+  decision = `John has higher BMI ${BMIJohn}`;
+}
+
+console.log(decision);
+
+//ver 2
+const markHigherBMI3: string =
+  BMIMark > BMIJohn
+    ? `Mark has higher BMI ${BMIMark}`
+    : `John has higher BMI ${BMIJohn}`;
+
+console.log(markHigherBMI3);
