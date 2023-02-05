af476e7f3e6a512afa2d9e4f25f9500d83b98df7
TheCodingBichon <93611775+TheCodingBichon@users.noreply.github.com>
2023-02-05 15:25:51 +0100

    Coding challenge #3 - completed

--- Fundamentals_1/src/Script.ts
+++ Fundamentals_1/src/Script.ts
@@ -36,3 +36,43 @@ const markHigherBMI3: string =
     : `John has higher BMI ${BMIJohn}`;
 
 console.log(markHigherBMI3);
+
+// Coding challenge #3
+
+const dolphinsScore: number[] = [96, 108, 89];
+const coalasScore: number[] = [88, 91, 110];
+
+const calcAvg = function (arr) {
+  return arr.reduce(function (prev, curr) {
+    return prev + curr;
+  }, 0);
+};
+
+// 1
+const avgScoreDolphins = calcAvg(dolphinsScore) / dolphinsScore.length;
+const avgScoreCoalas = calcAvg(coalasScore) / coalasScore.length;
+
+// 2
+if (avgScoreDolphins > avgScoreCoalas) {
+  console.log(`Dolphins won !!!`);
+} else if (avgScoreDolphins < avgScoreCoalas) {
+  console.log(`Coalas won !!!`);
+} else if (avgScoreCoalas === avgScoreDolphins) {
+  console.log(`Draw !!!`);
+}
+
+// 3
+
+if (avgScoreDolphins > avgScoreCoalas && avgScoreDolphins >= 100) {
+  console.log(`Dolphins won !!!`);
+} else if (avgScoreDolphins < avgScoreCoalas && avgScoreCoalas >= 100) {
+  console.log(`Coalas won !!!`);
+} else if (
+  avgScoreCoalas === avgScoreDolphins &&
+  avgScoreCoalas >= 100 &&
+  avgScoreDolphins >= 100
+) {
+  console.log(`Both win !!!`);
+} else {
+  console.log(`No one wins the trophy !!!`);
+}
