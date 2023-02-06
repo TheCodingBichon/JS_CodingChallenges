24897151bc89e0c2137f07731ee0208751941809
TheCodingBichon <93611775+TheCodingBichon@users.noreply.github.com>
2023-02-06 21:54:18 +0100

    Fundamentals_2 coding challenge #1 - completed

---
+++ Fundamentals_2/src/Script.ts
@@ -0,0 +1,28 @@
+// Coding challenge #1
+
+//Test 1
+const calcAverage = (a: number, b: number, c: number): number => {
+  return (a + b + c) / 3;
+};
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+
+const checkWinner = function (avgDolphins: number, avgKoalas: number) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win the trophy (${avgDolphins} vs. ${avgKoalas}) `);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win the trophy (${avgKoalas} vs. ${avgDolphins}) `);
+  } else {
+    console.log(`No team wins...`);
+  }
+};
+
+checkWinner(scoreDolphins, scoreKoalas);
+
+// Test 2
+
+const scoreDolphins2 = calcAverage(85, 54, 41);
+const scoreKoalas2 = calcAverage(23, 34, 27);
+
+checkWinner(scoreDolphins2, scoreKoalas2);
