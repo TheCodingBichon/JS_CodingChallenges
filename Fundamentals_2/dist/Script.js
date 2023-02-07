3d719b49464e16151a5aac84bc3e94439b649f48
TheCodingBichon <93611775+TheCodingBichon@users.noreply.github.com>
2023-02-07 22:09:14 +0100

    Fundamentals_2 - Coding challenge #2 - completed

--- Fundamentals_2/dist/Script.js
+++ Fundamentals_2/dist/Script.js
@@ -21,3 +21,27 @@ checkWinner(scoreDolphins, scoreKoalas);
 const scoreDolphins2 = calcAverage(85, 54, 41);
 const scoreKoalas2 = calcAverage(23, 34, 27);
 checkWinner(scoreDolphins2, scoreKoalas2);
+// Coding challenge #2
+const bill = 100;
+const tipLower = 0.15;
+const tipHigher = 0.2;
+// 1
+const calcTip = (bill) => {
+    return bill > 50 && bill < 300 ? bill * tipLower : bill * tipHigher;
+};
+const tipTest = calcTip(bill);
+console.log(tipTest);
+//2
+const bills = [125, 555, 44];
+//3
+const tips = bills.map(function (billValue) {
+    return calcTip(billValue);
+});
+console.log(tips);
+//4
+const totals = [
+    bills[0] + tips[0],
+    bills[1] + tips[1],
+    bills[2] + tips[2],
+];
+console.log(totals);
--- Fundamentals_2/src/Script.ts
+++ Fundamentals_2/src/Script.ts
@@ -26,3 +26,42 @@ const scoreDolphins2 = calcAverage(85, 54, 41);
 const scoreKoalas2 = calcAverage(23, 34, 27);
 
 checkWinner(scoreDolphins2, scoreKoalas2);
+
+// Coding challenge #2
+
+const bill: number = 100;
+
+const tipLower: number = 0.15;
+const tipHigher: number = 0.2;
+
+// 1
+
+const calcTip = (bill) => {
+  return bill > 50 && bill < 300 ? bill * tipLower : bill * tipHigher;
+};
+
+const tipTest: number = calcTip(bill);
+
+console.log(tipTest);
+
+//2
+
+const bills: number[] = [125, 555, 44];
+
+//3
+
+const tips: number[] = bills.map(function (billValue: number): number {
+  return calcTip(billValue);
+});
+
+console.log(tips);
+
+//4
+
+const totals: number[] = [
+  bills[0] + tips[0],
+  bills[1] + tips[1],
+  bills[2] + tips[2],
+];
+
+console.log(totals);
