e7d517a1b1c05fbddc3610946bf9efaa298021a4
TheCodingBichon <93611775+TheCodingBichon@users.noreply.github.com>
2023-02-08 21:51:16 +0100

    Fundamentals_2 - coding challenges #3 & #4 - comp

--- Fundamentals_2/dist/Script.js
+++ Fundamentals_2/dist/Script.js
@@ -45,3 +45,57 @@ const totals = [
     bills[2] + tips[2],
 ];
 console.log(totals);
+// Coding challenge #3
+const mark = {
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    calcBMI: function () {
+        this.bmi = this.mass / Math.pow(this.height, 2);
+        return this.bmi;
+    },
+};
+const john = {
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+    calcBMI: function () {
+        this.bmi = this.mass / Math.pow(this.height, 2);
+        return this.bmi;
+    },
+};
+mark.calcBMI();
+john.calcBMI();
+console.log(mark.bmi, john.bmi);
+if (mark.bmi > john.bmi) {
+    console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`);
+}
+else if (john.bmi > mark.bmi) {
+    console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`);
+}
+// Coding challenge #4
+const calcTipNew = (bill) => {
+    return bill > 50 && bill < 300 ? bill * tipLower : bill * tipHigher;
+};
+//1
+const billsNew = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+//2
+const tipsNew = [];
+const totalsNew = [];
+//3
+for (let i = 0; i < billsNew.length; i++) {
+    const tip = calcTipNew(billsNew[i]); //możemy użyć const ponieważ w każej kolejne iteracji zostanie on utworzony na nowo a nie zmieniany!
+    tipsNew.push(tip);
+    totalsNew.push(tip + billsNew[i]);
+}
+console.log(billsNew, tipsNew, totalsNew);
+// BONUS
+const calcAverageNew = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+};
+console.log(calcAverageNew([2, 3, 7]));
+console.log(calcAverageNew(totalsNew));
