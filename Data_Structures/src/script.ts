f47908df3d6c66b0972abc4f9bd26160c976ad7c
TheCodingBichon <93611775+TheCodingBichon@users.noreply.github.com>
2023-02-26 19:38:40 +0100

    Challenge #3 completed

--- Data_Structures/src/script.ts
+++ Data_Structures/src/script.ts
@@ -91,3 +91,45 @@ for (const [team, odd] of Object.entries(game.odds)) {
   const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
   console.log(`Odd of ${teamStr} ${odd}`);
 }
+
+// Challenge #3
+
+//Input data
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+//1
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+//2
+gameEvents.delete(64);
+
+//3
+console.log(
+  `An event happened, on average, ever ${90 / gameEvents.size} minutes`
+);
+
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+
+console.log(
+  `An event happened, on average, ever ${time / gameEvents.size} minutes`
+);
+
+//4
+for (const [min, event] of gameEvents) {
+  const half: string = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
